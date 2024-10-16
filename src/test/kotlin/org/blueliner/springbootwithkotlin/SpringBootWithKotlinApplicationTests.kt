c240f685d09a6be1d4cbdc9edb8e5a4b3741a19e
nevels <neevels@mail.ru>
2024-10-16 17:38:15 +0300

    init

---
+++ src/test/kotlin/org/blueliner/springbootwithkotlin/SpringBootWithKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.blueliner.springbootwithkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootWithKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
